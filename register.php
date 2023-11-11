51e765e8d45f01dabb0b3a9dda3ade299355edd5
yunweiwu <140036111+yunweiwu@users.noreply.github.com>
2023-11-11 15:42:49 +0800

    Update register.php

--- register.php
+++ register.php
@@ -1,32 +1,10 @@
- 
 <?php
-// 连接到数据库
-$servername = "localhost";
-$username = "username";
-$password = "password";
-$dbname = "database";
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+  // 获取用户输入的账号和密码
+  $username = $_POST['username'];
+  $password = $_POST['password'];
 
-$conn = new mysqli($servername, $username, $password, $dbname);
-
-// 检查连接是否成功
-if ($conn->connect_error) {
-    die("连接失败: " . $conn->connect_error);
+  // 将用户输入的账号密码保存到数据库中
+  // 这里可以使用数据库操作语句（如INSERT INTO）将数据插入到数据库表中
 }
-
-// 获取注册表单提交的数据
-$username = $_POST['username'];
-$password = $_POST['password'];
-$email = $_POST['email'];
-
-// 将数据插入数据库中的用户表
-$sql = "INSERT INTO users (username, password, email) VALUES ('$username', '$password', '$email')";
-
-if ($conn->query($sql) === TRUE) {
-    echo "注册成功！";
-} else {
-    echo "注册失败：" . $conn->error;
-}
-
-// 关闭数据库连接
-$conn->close();
 ?>
