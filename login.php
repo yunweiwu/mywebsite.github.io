92fc3af5012c102c6450fdebf71a87e73ee9b8e8
yunweiwu <140036111+yunweiwu@users.noreply.github.com>
2023-11-11 17:00:58 +0800

    Update login.php

--- login.php
+++ login.php
@@ -1,14 +1,25 @@
+<!-- login.php -->
 <?php
-if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-  // 获取用户输入的账号和密码
-  $username = $_POST['username'];
-  $password = $_POST['password'];
+session_start();
 
-  // 从数据库中查询账号和密码是否匹配
-  // 这里可以使用数据库操作语句（如SELECT）来查询数据库表中的数据
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $username = $_POST["username"];
+    $password = $_POST["password"];
 
-  // 如果账号密码匹配成功，则登录成功，进入登录后的界面
-  // 如果匹配失败，则登录失败，给出相应的提示信息
+    // 检查数据库中的用户名和密码是否匹配，这里假设已经有一个数据库表(users)保存了用户信息
+    $users = [
+        ["username" => "user1", "password" => "password1"],
+        ["username" => "user2", "password" => "password2"],
+    ];
 
+    foreach ($users as $user) {
+        if ($user["username"] == $username && $user["password"] == $password) {
+            $_SESSION["username"] = $username;
+            header("Location: message.php");
+            exit;
+        }
+    }
+
+    echo "Invalid username or password.";
 }
 ?>
